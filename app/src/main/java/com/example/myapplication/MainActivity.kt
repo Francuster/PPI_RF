158c56f0d71a32f6bfe2cbef4667762abaeff3b6
Alfonso lombardi <ajavierlombardi@gmail.com>
2024-05-01 12:36:36 -0300

    se agrega control de conexion

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if(deviceIsConnected(applicationContext)){
+            setContentView(R.layout.activity_main)
+        }else{
+            Toast.makeText(this, "No estás conectado a Internet", Toast.LENGTH_SHORT).show()
+            finish()
+        }
 
     }
     fun Siguiente(view : View){
@@ -16,4 +25,12 @@ class MainActivity: AppCompatActivity() {
         startActivity(intent)
 
     }
+
+    private fun deviceIsConnected(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork) ?: return false
+        return networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+    }
+
 }

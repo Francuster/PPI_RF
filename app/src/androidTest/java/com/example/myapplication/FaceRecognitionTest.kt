f03ffe390725cda9cdaba9f4072ca0007151870e
Franco Disabato <francuster1@gmail.com>
2024-05-09 23:17:56 -0300

    load vectors from in memory

--- app/src/androidTest/java/com/example/myapplication/FaceRecognitionTest.kt
+++ app/src/androidTest/java/com/example/myapplication/FaceRecognitionTest.kt
@@ -3,9 +3,17 @@ package com.example.myapplication
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Matrix
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.example.myapplication.activity.CameraxActivity
 import com.example.myapplication.service.FaceRecognitionV2
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.Face
+import com.google.mlkit.vision.face.FaceDetection
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Ignore
@@ -54,9 +62,16 @@ class FaceRecognitionTest{
                     val inputStream = assetManager.open("photos/$folderName/$fileName")
                     // Decode the InputStream into a Bitmap
                     val bitmap = BitmapFactory.decodeStream(inputStream)
-                    // Add the Bitmap to the list
-                    imagesList.add(bitmap)
-                    faceRecognitionV2.addFace(bitmap, folderName, context)
+                    // Create an InputImage from the bitmap
+                    val inputImage = InputImage.fromBitmap(bitmap, 0);
+                    //get face from input
+                    val cropedBitmap = faceDetection(inputImage)
+                    if(cropedBitmap != null){
+                        // Add the Bitmap to the list
+                        imagesList.add(cropedBitmap)
+                        faceRecognitionV2.addFace(cropedBitmap, folderName, context)
+                    }
+
                     // Close the InputStream
                     inputStream.close()
                 }
@@ -69,44 +84,46 @@ class FaceRecognitionTest{
 
     }
 
+
     // Method to load an image file as Bitmap
     private fun loadImageAsBitmap(imageFile: File): Bitmap {
         return BitmapFactory.decodeFile(imageFile.absolutePath)
     }
 
+    private fun faceDetection(inputImage: InputImage): Bitmap? {
+        var waiting = true;
+        var bitmap: Bitmap? = null
+        val faceDetector = FaceDetection.getClient()
+        val task: Task<List<Face>> = faceDetector.process(inputImage).addOnSuccessListener {
+            faces ->
+            run {
+                if (faces.size > 0) {
+                    val face = faces[0]
+                    val boundingBox = face.boundingBox
+
+                    // convert img to bitmap & crop img
+                    val faceBitmap = Bitmap.createBitmap(boundingBox.width(), boundingBox.height(), Bitmap.Config.ARGB_8888)
+                    val canvas = Canvas(faceBitmap)
+                    val matrix = Matrix()
+                    matrix.postTranslate(-boundingBox.left.toFloat(), -boundingBox.top.toFloat())
+                    canvas.drawBitmap(inputImage.bitmapInternal!!, matrix, null)
+                    bitmap = faceBitmap
+
+                    waiting = false;
 
 
-    fun loadImagesFromAssets2(context: Context): List<Pair<Bitmap, String>> {
-        val assetManager = context.assets
-        val imagesList = mutableListOf<Pair<Bitmap, String>>()
-
-        try {
-            // List all files in the "photos" folder
-            val folderList = assetManager.list("photos") ?: emptyArray()
-
-            // Iterate over each subfolder
-            for (folderName in folderList) {
-                // Get the list of files in the subfolder
-                val files = assetManager.list("photos/$folderName") ?: emptyArray()
-
-                // Iterate over each file in the subfolder
-                for (fileName in files) {
-                    // Open the file as InputStream
-                    val inputStream = assetManager.open("photos/$folderName/$fileName")
-                    // Decode the InputStream into a Bitmap
-                    val bitmap = BitmapFactory.decodeStream(inputStream)
-                    // Add the Bitmap and subfolder name to the list as a pair
-                    imagesList.add(Pair(bitmap, folderName))
-                    // Close the InputStream
-                    inputStream.close()
                 }
-            }
+                waiting = false;
 
-        } catch (e: IOException) {
-            e.printStackTrace()
+            }
+        }
+        while (waiting){
+            Thread.sleep(500)
         }
 
-        return imagesList
+        return bitmap
+
+
     }
 
 

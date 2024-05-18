326a6ef60aeed0019b4d41f3651db0b65678d264
Guillermo Arditti <guillermoarditti_97@hotmail.com>
2024-05-18 00:52:34 -0300

    Data Sync added, checking bugs

--- app/src/main/java/com/example/myapplication/activity/InicioSeguridadActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/InicioSeguridadActivity.kt
@@ -3,8 +3,11 @@ package com.example.myapplication.activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
+import com.example.myapplication.service.SendDataToBackend
+import com.example.myapplication.utils.deviceIsConnected
 
 
 class InicioSeguridadActivity: AppCompatActivity() {
@@ -12,17 +15,29 @@ class InicioSeguridadActivity: AppCompatActivity() {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.inicio_seguridad)
 
+
         }
 
     fun goToAnteEscanea(view: View) {
-
             val intent = Intent(applicationContext, AnteEscaneaActivity::class.java)
             startActivity(intent)
-
     }
 
-
-
+    //Bug cuando se toca el boton reconectar mas de una vez. Sospecha: Calls no se cierran.
+    fun reconectar(view: View){
+        if(deviceIsConnected(applicationContext)){
+            Toast.makeText(this, "Sincronizando...", Toast.LENGTH_SHORT).show()
+            val regRequest = SendDataToBackend(applicationContext)
+            if(regRequest.sendLocalRegs()){
+                Toast.makeText(this, "Sincronización exitosa/registros online actualizados", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "Nada para sincronizar", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(this, "No estás conectado a Internet", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
 
 
--- app/src/main/java/com/example/myapplication/service/SendDataToBackend.kt
+++ app/src/main/java/com/example/myapplication/service/SendDataToBackend.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication.service
 
 import android.content.Context
+import android.database.Cursor
 import android.widget.Toast
+import com.example.myapplication.database.Connection
 import com.example.myapplication.model.Log
+import com.example.myapplication.model.Registro
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.FormBody
@@ -10,11 +13,15 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 class SendDataToBackend (private val context: Context) {
 
-    fun  sendLog(log: Log) {
+
+    fun sendLog(log: Log) {
         // URL
 
         val url = "https://log3r.up.railway.app/api/authentication/logs"
@@ -28,11 +35,11 @@ class SendDataToBackend (private val context: Context) {
         // Crear el cuerpo de la solicitud HTTP
         val requestBody = FormBody.Builder()
             .add("horario", log.horario)
-            .add("nombre",log.nombre)
-            .add("apellido",log.apellido)
-            .add("dni",log.dni)
-            .add("estado",log.estado)
-            .add("tipo",log.tipo)
+            .add("nombre", log.nombre)
+            .add("apellido", log.apellido)
+            .add("dni", log.dni)
+            .add("estado", log.estado)
+            .add("tipo", log.tipo)
             .build()
 
         // Crea la solicitud POST
@@ -46,23 +53,97 @@ class SendDataToBackend (private val context: Context) {
             override fun onFailure(call: Call, e: IOException) {
                 // Maneja el fallo de la solicitud
                 e.printStackTrace()
-                Toast.makeText(context, "No se ha podido hacer el registro", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "No se ha podido hacer el registro", Toast.LENGTH_SHORT)
+                    .show()
 
             }
 
             override fun onResponse(call: Call, response: Response) {
                 // Maneja la respuesta del servidor
                 // Manejar la respuesta del servidor aquí
-                    // La solicitud fue exitosa //
-                    Toast.makeText(context, "Registro exitoso", Toast.LENGTH_SHORT).show()
-
-
+                // La solicitud fue exitosa //
+                Toast.makeText(context, "Registro exitoso", Toast.LENGTH_SHORT).show()
             }
-
-
-
         })
     }
 
+    fun sendLocalRegs(): Boolean {
+            val connection = Connection(context)
+            val db = connection.writableDatabase
+            val puntero = db.rawQuery("SELECT * FROM ingresos", null)
+
+            if (puntero.moveToFirst()) {
+                db.beginTransaction()
+                do {
+                    val reg = Registro(
+                        puntero.getString(puntero.getColumnIndexOrThrow("horario")),
+                        puntero.getString(puntero.getColumnIndexOrThrow("nombre")),
+                        puntero.getString(puntero.getColumnIndexOrThrow("apellido")),
+                        puntero.getString(puntero.getColumnIndexOrThrow("dni")),
+                        puntero.getString(puntero.getColumnIndexOrThrow("estado")),
+                        puntero.getString(puntero.getColumnIndexOrThrow("tipo")),
+                    )
+                    sendRegistro(reg)
+                } while (puntero.moveToNext())
+
+                db.execSQL("DELETE FROM ingresos")
+
+                db.setTransactionSuccessful()
+
+                puntero.close()
+                db.close()
+                return true
+            } else {
+                puntero.close()
+                db.close()
+                return false
+            }
+    }
+
+    fun sendRegistro(reg: Registro) {
+        // URL
+
+        val url = "https://log3r.up.railway.app/api/authentication/logs"
+        // CREAR CONEXION
+        val client = OkHttpClient().newBuilder()
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .writeTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .build()
+
+        val formato = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
+        val horario = formato.format(Date())
+
+        // Crear el cuerpo de la solicitud HTTP
+        val requestBody = FormBody.Builder()
+            .add("horario", reg.horario)
+            .add("nombre", reg.nombre)
+            .add("apellido", reg.apellido)
+            .add("dni", reg.dni)
+            .add("estado", reg.estado)
+            .add("tipo", reg.tipo)
+            .build()
+
+        // Crea la solicitud POST
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+        // Ejecuta la solicitud de forma asíncrona
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Maneja el fallo de la solicitud
+                e.printStackTrace()
+                Toast.makeText(context, "No se ha podido hacer el registro", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                // Maneja la respuesta del servidor
+                call.cancel() //Definir si sirve
+            }
+        })
+    }
 
 }

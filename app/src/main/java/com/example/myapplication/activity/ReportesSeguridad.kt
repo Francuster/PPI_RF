7719256f32d510669fa521ac889ca722b44f737d
estelucero <este.lucero2212@gmail.com>
2024-05-23 23:51:50 -0300

    Agrego onclick nav

--- app/src/main/java/com/example/myapplication/activity/ReportesSeguridad.kt
+++ app/src/main/java/com/example/myapplication/activity/ReportesSeguridad.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 import com.example.myapplication.utils.NetworkChangeService
@@ -11,5 +12,12 @@ class ReportesSeguridad: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.reporte_seguridad)
+    }
+
+    fun goToInicioSeguridad(view: View) {
+
+        val intent = Intent(applicationContext, InicioSeguridadActivity::class.java)
+        startActivity(intent)
+
     }
 }
